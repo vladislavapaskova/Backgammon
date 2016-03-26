5d0a9707b782cda1aa52a042f493ec7634ae2c0c
vladislavapaskova <pasko22v@mtholyoke.edu>
2016-03-26 12:06:43 -0400

    initial commit

---
+++ src/Board.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author vladislavapaskova
+ *
+ */
+public class Board {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
